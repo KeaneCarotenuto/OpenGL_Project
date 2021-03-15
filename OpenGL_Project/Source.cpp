2b3e1a8a44d928f36bc70714b748b151f1ff41c4
Keane <KeaneCarotenuto@gmail.com>
2021-03-16 11:20:24 +1300

    Moving Smooth Colour Shifting Triangle
    Moved buffer creation to init, made buffer Dynamic, smoothed the colour transition, and it can move and rotate.

--- OpenGL_Project/Source.cpp
+++ OpenGL_Project/Source.cpp
@@ -34,14 +34,6 @@ GLuint Program_ColorFade;
 GLuint VBO_Tri;
 GLuint VAO_Tri;
 
-// Vert //Indices
-GLfloat Vertices_Tri[] = {
-	// Position				// Color 
-	0.0f, 0.0f, 0.0f,		1.0f, 0.0f, 0.0f,	// Top Right
-	-0.5f, 0.8f, 0.0f,		0.0f, 1.0f, 0.0f,	// Top Left
-	0.5f, 0.8f, 0.0f,		0.0f, 0.0f, 1.0f,	// Bottom Center
-};
-
 vector3 pos;
 float ang = 0;
 float size = 1.0f;
@@ -123,7 +115,7 @@ void InitialSetup()
 
 	Program_FixedTri = ShaderLoader::CreateProgram("Resources/Shaders/TriangleVS.txt", "Resources/Shaders/FixedColor.txt");
 	//Program_FixedTri2 = ShaderLoader::CreateProgram("Resources/Shaders/TriangleVS.txt", "Resources/Shaders/FixedColor.txt");
-	//Program_ColorFade = ShaderLoader::CreateProgram("Resources/Shaders/VertexColorFade.txt", "Resources/Shaders/FixedColor.txt");
+	Program_ColorFade = ShaderLoader::CreateProgram("Resources/Shaders/TriangleVS.txt", "Resources/Shaders/VertexColorFade.txt");
 
 	glfwSetKeyCallback(window, key_callback);
 
@@ -134,8 +126,7 @@ void InitialSetup()
 	//Gen VBO for triangle
 	glGenBuffers(1, &VBO_Tri);
 	//copy our vertices array in a buffer for OpenGL to use
-	glBindBuffer(GL_ARRAY_BUFFER, VBO_Tri);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices_Tri), Vertices_Tri, GL_STATIC_DRAW);
+	EquiTriangle(pos, size, ang);
 
 	//Set the vertex attributes pointers (How to interperet Vertex Data)
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
@@ -190,13 +181,20 @@ void Render()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	glUseProgram(Program_FixedTri);
+	EquiTriangle(pos, size, ang);
+
+	glUseProgram(Program_ColorFade);
 	glBindVertexArray(VAO_Tri);
+
+	GLint CurrentTimeLoc = glGetUniformLocation(Program_ColorFade, "CurrentTime");
+	glUniform1f(CurrentTimeLoc, CurrentTime);
+
 	glDrawArrays(GL_TRIANGLES, 0, 3);
+
 	glBindVertexArray(0);
 	glUseProgram(0);
 
-	//EquiTriangle(pos, size, ang);
+	
 	//EquiTriangle(vector3{pos.x + 1, pos.y + 1, pos.z}, size, ang);
 
 	glfwSwapBuffers(window);
@@ -221,23 +219,7 @@ void Triangle(vector3 v1, vector3 c1, vector3 v2 , vector3 c2, vector3 v3 , vect
 		v3,  c3,
 	};
 
-	//Gen VAO for triangle
-	glGenVertexArrays(1, &VAO_Tri);
-	glBindVertexArray(VAO_Tri);
-
-	//Gen VBO for triangle
-	glGenBuffers(1, &VBO_Tri);
 	//copy our vertices array in a buffer for OpenGL to use
 	glBindBuffer(GL_ARRAY_BUFFER, VBO_Tri);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	//Set the vertex attributes pointers (How to interperet Vertex Data)
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)0);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), (GLvoid*)(3 * sizeof(GLfloat)));
-	glEnableVertexAttribArray(1);
-
-	glUseProgram(Program_FixedTri);
-	glDrawArrays(GL_TRIANGLES, 0, 3);
-	glUseProgram(0);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
 }
